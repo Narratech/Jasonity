9b1abff0b01e862e028ac5b79e656360e5d303dc
bastioncillo <alejga13@ucm.es>
2019-02-14 21:02:53 +0100

    Implementación completa del parser
    No sé si esto os valdrá, decidme si no para que lo cambie

--- Assets/Code/Logic/Parser.cs
+++ Assets/Code/Logic/Parser.cs
@@ -23,38 +23,73 @@ namespace Assets.Code.Logic
         public void Parsing(string location/*Recibir la localización del fichero por parámetro??*/)
         {
             //string file = "@" + location;
+            string aux, belief, subject, objective, @object, action, actionFocus;
             foreach(string line in File.ReadLines(@"E:\UnityProjects\
                 Jasonity-master\Assets\Code\Logic\TextFiles\Bob.txt"))
             {
                 switch (line[0])
                 {
                     case '!':
-                        //![decir](hello)
-                        string objective = line.Substring(1, line.IndexOf('(')-1);
+                        //[-]say(hello)
+                        aux = line.Remove(line.IndexOf('!'), line.IndexOf('!') + 1);
+                        //[say](hello)
+                        objective = aux.Substring(0, aux.IndexOf('('));
                         Console.WriteLine(objective);
-                        //[------](hello)
-                        line.Remove(0, line.IndexOf('('));
-                        Console.WriteLine(line);
+                        //[---](hello)
+                        @object = aux.Remove(0, aux.IndexOf('(') + 1);
+                        Console.WriteLine(@object);
                         //[hello])
-                        string @object = line.Substring(0, line.IndexOf(')') - 1);
-                                                    //say     //hello
+                        @object = @object.Substring(0, @object.IndexOf(')'));
+                        Console.WriteLine(@object);
+                        //say     //hello
                         objectives.Add(new Objective(objective, @object));
                         break;
 
                     case '+':
-                        //Pendiente (Misma idea)
+                        /*OBJETIVO DEL PLAN*/
+                        //[--]say(X):happy(bob)<-print(X)
+                        aux = line.Remove(line.IndexOf('+'), line.IndexOf('!') + 1);
+                        //[say](X):happy(bob)<-print(X)
+                        objective = aux.Substring(0, aux.IndexOf('('));
+                        //[---](X):happy(bob)<-print(X)
+                        @object = aux.Remove(0, aux.IndexOf('(') + 1);
+                        //[X]):happy(bob)<-print(X)
+                        @object = @object.Substring(0, @object.IndexOf(')'));
+                        //say       //X
+                        Objective o = new Objective(objective, @object);
+
+                        /*CONDICIÓN DEL PLAN*/
+                        //[--]happy(bob)<-print(X)
+                        aux = aux.Remove(0, aux.IndexOf(':') + 1);
+                        //[happy](bob)<-print(X)
+                        belief = aux.Substring(0, aux.IndexOf('('));
+                        //[------]bob)<-print(X)
+                        subject = aux.Remove(0, aux.IndexOf('(') + 1);
+                        //[bob])<-print(X)
+                        subject = subject.Substring(0, subject.IndexOf(')'));
+                        //happy   //bob
+                        Belief b = new Belief(belief, subject);
+
+                        /*FUNCIÓN DEL PLAN*/
+                        //[------]print(X)
+                        aux = aux.Remove(0, aux.IndexOf('-') + 1);
+                        //[print](X)
+                        action = aux.Substring(0, aux.IndexOf('('));
+                        //[------]X)
+                        actionFocus = aux.Remove(0, aux.IndexOf('(') + 1);
+                        //[X])
+                        actionFocus = actionFocus.Substring(0, actionFocus.IndexOf(')'));
+                        plans.Add(new Plan(o, b, action, actionFocus));
                         break;
 
                     default:
                         //[happy](bob)
-                        string belief = line.Substring(0, line.IndexOf('('));
-                        Console.WriteLine(belief);
+                        belief = line.Substring(0, line.IndexOf('('));
                         //[------]bob)
-                        line.Remove(0, line.IndexOf('('));
-                        Console.WriteLine(line);
+                        subject = line.Remove(0, line.IndexOf('(') + 1);
                         //[bob])
-                        string subject = line.Substring(0, line.IndexOf(')')-1);
-                                             //happy   //bob
+                        subject = subject.Substring(0, subject.IndexOf(')'));
+                        //happy   //bob
                         beliefs.Add(new Belief(belief, subject));
                         break;
                 }
--- Assets/Code/Logic/Plan.cs
+++ Assets/Code/Logic/Plan.cs
@@ -8,13 +8,26 @@ namespace Assets.Code.Logic
 {
     public class Plan : Term
     {
-        public Plan(string name) : base(name)
+        private Belief condition;
+        private Objective planObjective;
+        private string actionFocus;
+
+        public Plan(Objective objective, Belief belief, string name, string aFocus) : base(name)
         {
+            this.condition = belief;
+            this.planObjective = objective;
+            this.actionFocus = aFocus;
         }
 
         public override bool IsPlan()
         {
             return true;
         }
+
+        public Belief Condition { get => this.condition; }
+
+        public Objective PlanObjective { get => this.planObjective; }
+
+        public string AFocus { get => this.actionFocus; }
     }
 }
