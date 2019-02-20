39acd29ddfb209241be4a1bd32d74f6d7a9d158a
Juan <koldur.c@gmail.com>
2019-02-20 18:02:00 +0100

    Button to add prefab to scene done
    Test pending, form still a placeholder

---
+++ Assets/Editor/CustomWindow.cs
@@ -0,0 +1,47 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEditor;
+
+ 
+ public class CustomWindow : EditorWindow {
+ 
+     Vector3 vec3 = new Vector3(0,0,0);
+     string objname = "JASONIY_MASTER";
+     string fieldOne = "";
+	 string fieldTwo = "";
+	 string fieldThree = "";
+ 
+     [MenuItem ("Window/My Custom Window")]
+     static void ShowWindow () {
+		 Debug.Log(GameObject.Find("JASONIY_MASTER"));
+		 if(GameObject.Find("JASONIY_MASTER")!=null){
+			// EditorGUILayout.HelpBox("You already have that object on the current scene!", MessageType.Warning);
+		 }else{
+         	EditorWindow.GetWindow(typeof(CustomWindow));
+		 }
+     }
+ 
+     void OnInspectorUpdate () {
+
+         Repaint ();
+     }
+     void OnGUI()
+     {
+         fieldOne = EditorGUILayout.TextField("Name for new object: ", fieldOne);
+         fieldTwo = EditorGUILayout.TextField("Name for new object: ", fieldTwo);
+         fieldThree = EditorGUILayout.TextField("Name for new object: ", fieldThree);
+ 
+         if (fieldOne!="" && fieldTwo!="" && fieldThree != "")    {
+             if (GUILayout.Button( "Add to scene"))
+             {
+                 
+                 Object n = Resources.Load("prefab");
+				 GameObject tempobj = (GameObject)Instantiate(n, new Vector3(0,0,0), Quaternion.identity);
+				 tempobj.name = "JASONIY_MASTER";
+				 this.Close();
+             }
+         } else {
+             EditorGUILayout.LabelField ( "You must select an object and give it a name first");
+         }
+     }
+ }
---
+++ Assets/Editor/JasonityAgents.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class JasonityAgents : EditorWindow
+{
+    string agentsPath;
+    bool groupEnabled, groupEnabled2;
+    bool myBool = true, myBool2 = false;
+    float myFloat = 1.23f, myFloat2 = 1.70f;
+    int agentCounter = 0;
+    
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Window/Jasonity - Agents")]
+
+    
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(JasonityAgents));
+    }
+    
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        for(int i = 0; i < 4; i++){
+            string myString = "Agente 0" + i;
+            GUILayout.Label(myString, EditorStyles.boldLabel);
+            myString = EditorGUILayout.TextField("Agent Name", myString);
+            //GUILayout.Button("Rename");
+            groupEnabled = EditorGUILayout.BeginToggleGroup ("Generate on Scene", groupEnabled);
+                myBool = EditorGUILayout.Toggle ("  Fast Thinking", myBool);
+                myFloat = EditorGUILayout.Slider ("  Heigth", myFloat, -3, 3);
+            EditorGUILayout.EndToggleGroup();
+            if(GUILayout.Button("Edit")){
+                string lPath = "frayAlejandro.asl";
+                foreach (var lAssetPath in AssetDatabase.GetAllAssetPaths())
+                {
+                    if (lAssetPath.EndsWith(lPath)){
+                        Debug.Log("found");
+                        var lScript = (MonoScript)AssetDatabase.LoadAssetAtPath(lAssetPath, typeof(MonoScript));
+                        if (lScript != null)
+                        {
+                            Debug.Log(lAssetPath);
+                            AssetDatabase.OpenAsset(lScript);
+                            break;
+                        }
+                    }
+                }
+                
+                /*string path ="Assets/Jasonity/Agents/frayAlejandro.asl";
+                var lScript = (MonoScript)AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
+                Debug.Log(lScript);
+                AssetDatabase.OpenAsset(lScript);*/
+            }   
+            Rect rect = EditorGUILayout.GetControlRect(false, 1 );
+            rect.height = 1;
+            EditorGUI.DrawRect(rect, new Color ( 0.5f,0.5f,0.5f, 1 ) );
+            agentCounter++;
+        }
+        if(GUILayout.Button("Create new agent")){
+            Debug.Log("Hey");
+            string myString = "Agente 0" + agentCounter;
+            GUILayout.Label(myString, EditorStyles.boldLabel);
+            myString = EditorGUILayout.TextField("New Agent Name", myString);
+            groupEnabled = EditorGUILayout.BeginToggleGroup ("Generate on Scene", groupEnabled);
+                myBool = EditorGUILayout.Toggle ("  Fast Thinking", myBool);
+                myFloat = EditorGUILayout.Slider ("  Heigth", myFloat, -3, 3);
+            EditorGUILayout.EndToggleGroup();
+
+            Rect rect = EditorGUILayout.GetControlRect(false, 1 );
+            rect.height = 1;
+            EditorGUI.DrawRect(rect, new Color ( 0.5f,0.5f,0.5f, 1 ) );
+            agentCounter++;
+        }
+        EditorGUILayout.EndVertical();
+    }
+    void OnEnable(){
+        agentsPath = Application.dataPath;
+    }
+}
