bf3bfc42d9b9f8eef1be49746e63470ff896bd71
Juan Gómez-Martinho <koldur.c@gmail.com>
2019-02-26 18:07:58 +0100

    Boton de creación de MAS hecho

---
+++ Assets/Editor/JasonityConsole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class JasonityConsole : EditorWindow {
+
+	[MenuItem("Window/Jasonity - Console")]
+	public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(JasonityConsole));
+    }
+    
+    void OnGUI()
+    {
+		
+	}
+}
