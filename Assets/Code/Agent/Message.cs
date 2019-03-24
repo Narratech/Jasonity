b4509a9902f1898b6f81587522244cf63a7dac21
Athoka <irene_gonve@hotmail.com>
2019-03-24 12:47:19 +0100

    Added Message.cs for the mail between agents management

---
+++ Assets/Code/Agent/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Agent
+{
+    class Message
+    {
+        internal object GetPropCont()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetInReplyTo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
