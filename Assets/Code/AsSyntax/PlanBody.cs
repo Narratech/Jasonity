e19a91ff559ce76e011173eb471f8c3d7f2c7ff8
jbasdom <36700003+jbasdom@users.noreply.github.com>
2019-04-07 13:02:28 +0200

    Added BodyType enum in PlanBody

--- Assets/Code/AsSyntax/PlanBody.cs
+++ Assets/Code/AsSyntax/PlanBody.cs
@@ -29,6 +29,35 @@
 
     public class BodyType
     {
-        internal static BodyType none;
+        public readonly static BodyType none;
+        public readonly static BodyType action;
+        public readonly static BodyType internalAction;
+        public readonly static BodyType achieve;
+        public readonly static BodyType test;
+        public readonly static BodyType addBel;
+        public readonly static BodyType addBelNewFocus;
+        public readonly static BodyType addBelBegin;
+        public readonly static BodyType addBelEnd;
+        public readonly static BodyType delBel;
+        public readonly static BodyType delBelNewFocus;
+        public readonly static BodyType delAddBel;
+        public readonly static BodyType achieveNF;
+        public readonly static BodyType constraint;
+
+        public override string ToString()
+        {
+            if (this == none || this == action || this == internalAction || this == constraint) return "";
+            else if (this == achieve) return "!";
+            else if (this == test) return "?";
+            else if (this == addBel) return "+";
+            else if (this == addBelNewFocus) return "++";
+            else if (this == addBelBegin) return "+<";
+            else if (this == addBelEnd) return "+>";
+            else if (this == delBel) return "-";
+            else if (this == delBelNewFocus) return "--";
+            else if (this == delAddBel) return "-+";
+            else if (this == achieveNF) return "!!";
+            else return null;
+        }
     }
 }
