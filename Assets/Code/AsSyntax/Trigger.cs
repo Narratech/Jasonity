b173b3db137128d469b1ffece59b624cb8c58994
Athoka <irene_gonve@hotmail.com>
2019-04-07 10:46:27 +0200

    Trigger done. Theres errors because of other classes

--- Assets/Code/AsSyntax/Trigger.cs
+++ Assets/Code/AsSyntax/Trigger.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Assets.Code.Logic.AsSyntax;
+using Assets.Code.ReasoningCycle;
+using BDIManager.Beliefs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +13,200 @@ namespace Assets.Code.Logic
     {
         private TEOperator op = TEOperator.add;
         private TEType type = TEType.belief;
-        private Literal l;
+        private Literal literal;
+        private bool isTerm = false;
 
         public Trigger(TEOperator op, TEType t, Literal l) : base("te", 0)
         {
-            this.l = l;
+            this.literal = l;
             this.op = op;
             this.type = t;
             SetTrigOp(op);
             SetSrcInfo(l.GetSrcInfo());
+        } 
+
+        public override int GetArity()
+        {
+            return 2;
+        }
+         
+        public override Term GetTerm(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    {
+                        return new StringTermImpl(op.ToString() + type.ToString());
+                    }
+                case 1:
+                    {
+                        return literal;
+                    } 
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        public override void SetTerm(int i, Term t)
+        {
+            switch (i)
+            {
+                case 0:
+                    {
+                        //logger.warning("setTerm(i,t) for i=0 -- the operator -- , IS NOT IMPLEMENTED YET!!!");
+                        break;
+                    } 
+                case 1:
+                    {
+                        literal = (Literal)t;
+                        break;
+                    }
+            }
+        }
+
+        public void SetTrigOp(TEOperator o)
+        {
+            op = o;
+            predicateIndicatorCache = null;
+        }
+
+        public bool SameType(Trigger e)
+        {
+            return op == e.op && type == e.type;
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (o != null && o.GetType() == typeof(Trigger)) {
+                Trigger t = (Trigger)o;
+                return (op == t.op && type == t.type && literal.Equals(t.getLiteral()));
+            }
+            return false;
+        }
+
+        public bool IsAchvGoal()
+        {
+            return type == TEType.achieve;
+        }
+
+        public bool IsGoal()
+        {
+            return type == TEType.achieve || type == TEType.test;
+        }
+
+        public bool IsMetaEvent()
+        {
+            return op == TEOperator.goalState;
+        }
+
+        public TEOperator GetOperator()
+        {
+            return op;
+        }
+
+        public TEType GetType()
+        {
+            return type;
+        }
+
+        public bool IsAddition()
+        {
+            return op == TEOperator.add;
+        }
+
+        public override Trigger Clone()
+        {
+            Trigger c = new Trigger(op, type, literal.Copy());
+            c.predicateIndicatorCache = predicateIndicatorCache;
+            c.isTerm = isTerm;
+            return c;
+        }
+
+        public override Trigger Capply(Unifier u)
+        {
+            Trigger c = new Trigger(op, type, (Literal)literal.Capply(u));
+            c.predicateIndicatorCache = predicateIndicatorCache;
+            c.isTerm = isTerm;
+            return c;
+        }
+
+        public override PredicateIndicator GetPredicateIndicator()
+        {
+            if (predicateIndicatorCache == null)
+            {
+                predicateIndicatorCache = new PredicateIndicator(literal.GetNS(), op.ToString() + type + literal.GetFunctor(), literal.GetArity());
+            }
+            return predicateIndicatorCache;
+        }
+
+        public Literal GetLiteral()
+        {
+            return literal;
+        }
+
+        public void SetLiteral(Literal literal)
+        {
+            this.literal = literal;
+            predicateIndicatorCache = null;
+        }
+
+        public void SetAsTriggerTerm(bool b)
+        {
+            isTerm = b;
+        }
+
+        public override String ToString()
+        {
+            String b, e;
+            if (isTerm)
+            {
+                b = "{ ";
+                e = " }";
+            }
+            else
+            {
+                b = "";
+                e = "";
+            }
+            return b + op + type + literal + e;
+        }
+
+        public static Trigger TryToGetTrigger(Term t)
+        {
+            if (t.GetType() == typeof(Trigger)) 
+            {
+                return (Trigger) t;
+            }
+            if (t.IsPlanBody())
+            {
+                PlanBody p = (PlanBody)t;
+                if (p.GetPlanSize() == 1)
+                {
+                    TEOperator op = null;
+                    if (p.GetBodyType() == BodyType.addBel)
+                    {
+                        op = TEOperator.add;
+                    } 
+                    else if (p.GetBodyType() == BodyType.delBel)
+                    {
+                        op = TEOperator.del;
+                    }
+                    if (op != null)
+                    {
+                        Literal l = ((Literal)p.GetBodyTerm().Clone()).ForceFullLiteralImpl();
+                        l.DelAnnot(BeliefBase.TSelf);
+                        return new Trigger(op, TEType.belief, l);
+
+                    }                       
+                }   
+            }   
+            if (t.IsString())
+            {
+                return ASSyntax.ParseTrigger(((StringTerm) t).GetString());
+            }
+            return null;
         }
     }
 
