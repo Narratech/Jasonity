f9933326c4c47a48a32894e191b0f4340699ce5d
bastioncillo <alejga13@ucm.es>
2019-04-14 19:27:04 +0200

    More stdlib classes added

--- Assets/Code/Stdlib/FailGoalStdLib.cs
+++ Assets/Code/Stdlib/FailGoalStdLib.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assets.Code.AsSyntax;
+using Assets.Code.ReasoningCycle;
+using BDIManager.Intentions;
 
 namespace Assets.Code.Stdlib
 {
-    class FailGoalStdLib
+    public class FailGoalStdLib: SucceedGoalStdLib
     {
+        public override object Execute(Reasoner reasoner, Unifier un, ITerm[] args)
+        {
+            CheckArguments(args);
+            FindGoalAndDrop(reasoner, (Literal)args[0], un);
+            return true;
+        }
+
+        /* returns: >0 the intention was changed
+         *           1 = intention must continue running
+         *           2 = fail event was generated and added in C.E
+         *           3 = simply removed without event
+         */
+        public override int DropGoal(Intention i, Trigger g, Reasoner rs, Unifier un)
+        {
+            if (i != null)
+            {
+                if (i.DropGoal(g ,un))
+                {
+                    //notify listener
+                    if (rs.HasGoalListener())
+                    {
+                        foreach (GoalListener gl in rs.GetGoalListeners())
+                        {
+                            gl.GoalFailed(g);
+                        }
+                    }
+
+                    //generate failure event
+                    Event failEvent = TaskScheduler.FindEventForFailure(i, g); //find fail event for the goal just dropped
+                    if (failEvent != null)
+                    {
+                        failEvent = new Event(failEvent.GetTrigger().Capply(un), failEvent.GetIntention());
+                        rs.GetCircumstance().AddEvent(failEvent);
+                        return 2;
+                    }
+                    else //i is finished or without failure plan
+                    {
+                        if (rs.HasGoalListener())
+                        {
+                            foreach (GoalListener gl in rs.GetGoalListeners())
+                            {
+                                gl.GoalFinished(g, FinishStates.unachived);
+                            }
+                        }
+                        i.Fail(rs.GetCircumstance());
+                        return 3;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public override void DropGoalInEvent(Reasoner rs, Event e, Intention i)
+        {
+            e.GetTrigger().SetTrigOp(TEOperator.del);
+        }
     }
 }
---
+++ Assets/Code/Stdlib/SucceedGoalStdLib.cs
@@ -0,0 +1,215 @@
+﻿using Assets.Code.Agent;
+using Assets.Code.AsSyntax;
+using Assets.Code.BDIManager;
+using Assets.Code.Exceptions;
+using Assets.Code.ReasoningCycle;
+using BDIManager.Intentions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Stdlib
+{
+    public class SucceedGoalStdLib:InternalAction
+    {
+        public override int GetMinArgs()
+        {
+            return 1;
+        }
+
+        public override int GetMaxArgs()
+        {
+            return 2;
+        }
+
+        protected override void CheckArguments(ITerm[] args)
+        {
+            base.CheckArguments(args);
+            if (!args[0].IsLiteral())
+            {
+                throw JasonityException.CreateWrongArgument(this, "first argument must be a literal");
+            }
+        }
+
+        public override object Execute(Reasoner reasoner, Unifier un, ITerm[] args)
+        {
+            CheckArguments(args);
+            FindGoalAndDrop(reasoner, (Literal)args[0], un);
+            return true;
+        }
+
+        public void FindGoalAndDrop(Reasoner rs, Literal l, Unifier un)
+        {
+            Trigger g = new Trigger(TEOperator.add, TEType.achieve, l);
+            Circumstance C = rs.GetCircumstance();
+            Unifier bak = un.Clone();
+
+            IEnumerator<Intention> itinit = C.GetRunningIntentionPlusAtomic();
+            while (itinit.MoveNext())
+            {
+                Intention i = itinit.Current;
+                if (DropGoal(i, g, rs, un) > 1)
+                {
+                    C.DropRunningIntention(i);
+                    un = bak.Clone();
+                }
+            }
+
+            // dropping the current intention?
+            DropGoal(C.GetSelectedIntention(), g, rs, un);
+            un = bak.Clone();
+
+            //dropping G in Events
+            IEnumerator<Event> ie = C.GetEventsPlusAtomic();
+            while (ie.MoveNext())
+            {
+                Event e = ie.Current;
+                //Test in the intention
+                Intention i = e.GetIntention();
+                int r = DropGoal(i, g, rs, un);
+                if (r > 0)
+                {
+                    C.RemoveEvent(e);
+                    if (r == 1)
+                    {
+                        C.ResumeIntention(i);
+                    }
+                    un = bak.Clone();
+                }
+                else
+                {
+                    //Test in the event
+                    Trigger t = e.GetTrigger();
+                    if (i != Intention.emptyInt && !i.IsFinished())
+                    {
+                        t = t.Capply(i.Peek().GetUnif());
+                    }
+                    if (un.Unifies(g, t))
+                    {
+                        DropGoalInEvent(rs, e, i);
+                        un = bak.Clone();
+                    }
+                }
+            }
+
+            //dropping G in Pending Events
+            foreach (string ek in C.GetPendingEvents().KeySet())
+            {
+                //Test in the intention
+                Event e = C.GetPendingEvents().Get(ek);
+                Intention i = e.GetIntention();
+                int r = DropGoal(i, g, rs, un);
+                if (r > 0)
+                {
+                    C.RemovePendingEvent(ek);
+                    if (r == 1)
+                    {
+                        C.ResumeIntention(i);
+                    }
+                    un = bak.Clone();
+                }
+                else
+                {
+                    //test in the event
+                    Trigger t = e.GetTrigger();
+                    if (i != Intention.emptyInt && !i.IsFinished())
+                    {
+                        t = t.Capply(i.Peek().GetUnif());
+                    }
+                    if (un.Unifies(g, t))
+                    {
+                        DropGoalInEvent(rs, e, i);
+                        un = bak.Clone();
+                    }
+                }
+            }
+
+            //Dropping from pending Actions
+            foreach (ActionExec a in C.GetPendingActions().Values())
+            {
+                Intention i = a.GetIntention();
+                int r = DropGoal(i, g, rs, un);
+                if (r > 0) //i was changed
+                {
+                    C.RemovePendingAction(i.GetID());   // remove i from PA
+                    if (r == 1)                         // i must continue running
+                    {
+                        C.ResumeIntention(i);           // and put the intention back in I
+                    }                                   // if r > 1, the event was generated and i will be back soon
+                    un = bak.Clone();
+                }
+            }
+
+            //Dropping from pending intentions
+            foreach(Intention i in C.GetPendingIntentions().Values())
+            {
+                int r = DropGoal(i, g, rs, un);
+                if (r > 0)
+                {
+                    C.RemovePendingIntention(i.GetID());
+                    if (r == 1)
+                    {
+                        C.ResumeIntention(i);
+                    }
+                    un = bak.Clone();
+                }
+            }
+        }
+
+        /* returns: >0 the intention was changed
+         *           1 = intention must continue running
+         *           2 = fail event was generated and added in C.E
+         *           3 = simply removed without event
+         */
+        public virtual int DropGoal(Intention i, Trigger g, Reasoner rs, Unifier un)
+        {
+            if (i != null && i.DropGoal(g, un))
+            {
+                if (rs.HasGoalListener())
+                {
+                    foreach (GoalListener gl in rs.GetGoalListeners())
+                    {
+                        gl.GoalFinished(g, FinishStates.achived);
+                    }
+                }
+                //continue the intention
+                if (!i.IsFinished())
+                {
+                    if (rs.GetCircumstance().GetSelectedIntention() != i)
+                    {
+                        i.Peek().RemoveCurrentStep();
+                    }
+                    rs.ApplyClrInt(i);
+                    return 1;
+                }
+                else
+                {
+                    rs.ApplyClrInt(i);
+                    return 3;
+                }
+            }
+            return 0;
+        }
+
+        public virtual void DropGoalInEvent(Reasoner rs, Event e, Intention i)
+        {
+            Circumstance C = rs.GetCircumstance();
+            C.RemoveEvent(e);
+            if (i != null)
+            {
+                if (rs.HasGoalListener())
+                {
+                    foreach (GoalListener gl in rs.GetGoalListeners())
+                    {
+                        gl.GoalFinished(e.GetTrigger(), FinishStates.achived);
+                    }
+                    i.Peek().RemoveCurrentStep();
+                    rs.ApplyClrInt(i);
+                    C.AddRunningIntention(i);
+                }
+            }
+        }
+    }
+}
