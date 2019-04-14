0bb8686cf46110bc38db005e22515fcac1e47a1d
Athoka <irene_gonve@hotmail.com>
2019-04-14 17:44:04 +0200

    Deleted duplicated message and added namespace to arith function and message

--- Assets/Code/AsSemantics/ArithFunction.cs
+++ Assets/Code/AsSemantics/ArithFunction.cs
@@ -1,15 +1,18 @@
 ﻿using Assets.Code.AsSyntax;
 using Assets.Code.ReasoningCycle;
 
-public class ArithFunction
+namespace Assets.Code.AsSemantics
 {
-    public virtual string GetName() => GetType().Name; // Not sure if this is right
+    public class ArithFunction
+    {
+        public virtual string GetName() => GetType().Name; // Not sure if this is right
 
-    public virtual bool AllowUngroundTerms() => false;
+        public virtual bool AllowUngroundTerms() => false;
 
-    public virtual bool CheckArity(int a) => true;
+        public virtual bool CheckArity(int a) => true;
 
-    public virtual double Evaluate(Reasoner r, ITerm[] args) => 0;
+        public virtual double Evaluate(Reasoner r, ITerm[] args) => 0;
 
-    public override string ToString() => "function "+GetName();
+        public override string ToString() => "function " + GetName();
+    }
 }
