edb74c8f426f48b03ada30aa90b6cd84c6dd0962
jbasdom <36700003+jbasdom@users.noreply.github.com>
2019-04-14 17:54:22 +0200

    StructureWrapperForLiteral done

--- Assets/Code/BDIManager/Beliefs/StructureWrapperForLiteral.cs
+++ Assets/Code/BDIManager/Beliefs/StructureWrapperForLiteral.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 
 namespace Assets.Code.Logic
 {
+    // Uses EqualsAsTerm for equals
     public class StructureWrapperForLiteral
     {
         private Literal l;
 
-        public StructureWrapperForLiteral(Literal l)
-        {
-            this.l = l;
-        }
+        public StructureWrapperForLiteral(Literal l) => this.l = l;
+
+        public int HashCode() => l.GetHashCode();
+
+        public override bool Equals(object o) => o is StructureWrapperForLiteral && l.EqualsAsStructure(((StructureWrapperForLiteral)o).l);
+
+        public override string ToString() => l.ToString();
+
+        public int CompareTo(StructureWrapperForLiteral o) => l.CompareTo(o.l);
+
+        public Literal GetLiteral() => l;
     }
 }
 
