5c2125ad6b0465082239ac9167871869acfc6f4c
Juan <koldur.c@gmail.com>
2019-05-05 20:44:44 +0200

    BaseCentralisedMAS done

--- Assets/Code/Infra/BaseCentralisedMAS.cs
+++ Assets/Code/Infra/BaseCentralisedMAS.cs
@@ -5,5 +5,56 @@ using Assets.Code.AsSyntax;
 
 public class BaseCentralisedMAS{
 
+    protected static BaseCentralisedMAS runner        = null;
+    protected static String             urlPrefix     = "";
+    protected static boolean            readFromJAR   = false;
+    protected static boolean            debug         = false;
+
+    protected getEnvironmentInfraTier        env         = null;
+    //protected CentralisedExecutionControl   control     = null; //No tiene uso?
+    protected IMap<String,CentralisedAgArch> ags         = new ConcurrentHashMap<String,CentralisedAgArch>();
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public static BaseCentralisedMAS getRunner() {
+        return runner;
+    }
+
+    public RuntimeServicesInfraTier getRuntimeServices() {
+        return new CentralisedRuntimeServices(runner);
+    }
+
+    /* 
+    public CentralisedExecutionControl getControllerInfraTier() {
+        return control;
+    }
+*/
+    public CentralisedEnvironment getEnvironmentInfraTier() {
+        return env;
+    }
+
+    public void addAg(CentralisedAgArch ag) {
+        ags.Put(ag.getAgName(), ag);
+    }
+    public CentralisedAgArch delAg(String agName) {
+        return ags.Remove(agName);
+    }
+
+    public CentralisedAgArch getAg(String agName) {
+        return ags.Get(agName);
+    }
+
+    public IMap<String,CentralisedAgArch> getAgs() {
+        return ags;
+    }
+    public int getNbAgents() {
+        return ags.size();
+    }
+
+    public void finish(){
+         Application.Quit();
+    }
     
 }
