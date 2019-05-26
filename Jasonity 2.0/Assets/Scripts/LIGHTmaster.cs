334fd2403d8cb1f21812b82fbbad955b41c07221
Juan Gómez-Martinho <koldur.c@gmail.com>
2019-05-26 15:38:15 +0200

    División de proyectos

---
+++ Jasonity 2.0/Assets/Scripts/LIGHTmaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LIGHTmaster : MonoBehaviour
+{
+    public GameObject light;
+    public GameObject apagar;
+    public GameObject encender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        apagar.SetActive(false);
+        encender.SetActive(false);
+        StartCoroutine("Switch");
+    }
+
+    IEnumerator Switch(){
+        while(true){
+            apagar.SetActive(true);
+            yield return new WaitForSeconds(3);
+            light.SetActive(false);
+            apagar.SetActive(false);
+            yield return new WaitForSeconds(3);
+            encender.SetActive(true);
+            yield return new WaitForSeconds(3);
+            light.SetActive(true);
+            encender.SetActive(false);
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
