8fe6532a379ad131097fa99f08864b34aebb3ef9
alvarocual <alvarocual@gmail.com>
2019-05-26 22:00:28 +0200

    Update Reasoner

--- Jasonity 2.0/Assets/Code/BDI/Reasoner.cs
+++ Jasonity 2.0/Assets/Code/BDI/Reasoner.cs
@@ -4,31 +4,39 @@ namespace Assets.Code.BDI
 {
     public class Reasoner:IRunnable //This might not be a runnable 
     {
-        public Reasoner()
-        {
+        private Agent ag;
 
+        //Constructora del razonador con el agente???
+        public Reasoner(Agent agent)
+        {
+            ag = agent;
         }
         
         public void Perceive()
         {
             //Makes the agent perceive the environment
+            ag.GetBeliefBase(); //??
         }
 
         public void UpdateBeliefs()
         {
             //Makes the agent update it's belief base
+            ag.UpdateBeliefBase();
+
         }
 
         public Intention SelectPlan()
         {
             //Retrieves the relevant plans, determines the applicable plan
             //Selects one plan. 
-            return null;
+
+            return ag.getCurrentPlan(); ;
         }
 
         public void Act(Intention plan)
         {
             //Gets the plan body of the plan, and enqueues the actions in the executor
+            plan.GetPlanBody();
         }
 
         public void Run()
